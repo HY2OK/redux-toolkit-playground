3466b77807318926fddf57f96bbbd8733fa38bbe
jun_ <gurwns232@naver.com>
2024-03-02 20:26:07 +0900

    feat: add incrementAmount

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,15 +1,28 @@
-import { decrement, increment } from '../../features/counter/counterSlice'
+import { ChangeEvent, useState } from 'react'
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from '../../features/counter/counterSlice'
 import { useAppDispatch, useAppSelector } from '../../hook'
 
 const Counter = () => {
   const counter = useAppSelector((state) => state.counter.value)
   const dispatch = useAppDispatch()
 
+  const [amount, setAmount] = useState(2)
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value))
+  }
+
   return (
     <div>
       <div>{counter}</div>
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <input type="number" name="num" id="num" onChange={handleInput} value={amount} />
+      <button onClick={() => dispatch(incrementByAmount(amount))}>add</button>
     </div>
   )
 }
