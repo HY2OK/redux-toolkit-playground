347f4f53fd1643da349e3faa9adb8f13bda1ce86
jun_ <gurwns232@naver.com>
2024-03-19 18:32:48 +0900

    feat: rtk-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css'
 import Counter from './components/Counter/Counter'
+import { useGetPokemonByNameQuery } from './features/pokemon/pokemonApi'
 
 function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+
+  console.log('data', data)
+  console.log('error', error)
+  console.log('isLoading', isLoading)
   return (
     <div className="App">
       <Counter />
--- src/store.tsx
+++ src/store.tsx
@@ -1,11 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counter from './features/counter/counterSlice'
+import { pokemonApi } from './features/pokemon/pokemonApi'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   reducer: {
     counter,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
